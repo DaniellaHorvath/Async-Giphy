f5c9e14c951f99277708b47e9c3407b7858c7327
Daniella Horvath <daniella.horvath@cx3yw2l2fy.home>
2024-07-29 16:44:03 +0100

    Declared an asynchronous function getImage(query).

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 require('dotenv').config();
 
 // Print out value of API key stored in .env file
-console.log(process.env.API_KEY)
+// console.log(process.env.API_KEY)
 
-const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=BqYcyLO0Sa7rtEuaiu4dN9U2cb8Ws1uD&q=REQUIRED&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
+const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=dog&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
+
+async function getImages(query){
+    const response = await fetch(query);
+    const data = await response.json();
+    
+    const imageUrl = data.data[0].url
+
+    console.log('Dog image: ', imageUrl);
+    
+}
 
