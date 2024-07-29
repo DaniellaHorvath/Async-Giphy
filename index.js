9160b24549e308400cfd0b0eef2dd5bc92dd5256
Daniella Horvath <daniella.horvath@cx3yw2l2fy.home>
2024-07-29 15:40:38 +0100

    Created API_KEY from giphy/ added endpont to the index.js file

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ require('dotenv').config();
 
 // Print out value of API key stored in .env file
 console.log(process.env.API_KEY)
+
+const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=BqYcyLO0Sa7rtEuaiu4dN9U2cb8Ws1uD&q=REQUIRED&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
+
